1d0f39fabcb218f908f23fc6973632c72260457a
bolthorn91 <bolthorn91@gmail.com>
2023-06-18 00:57:11 +0100

    Add send email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MailModule } from './mail/mail.module';
+import { MailModule } from '../mail/mail.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
